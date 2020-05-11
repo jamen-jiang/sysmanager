e9929b747c114dc682a960de8f672a47441ac4d4
jamen-jiang <365701919@qq.com>
2020-05-11 14:48:39 +0800

    改了一下

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="less">
-html,body,#app,.el-container{
-    /*设置内部填充为0，几个布局元素之间没有间距*/
-    padding: 0px;
-      /*外部间距也是如此设置*/
-    margin: 0px;
-    /*统一设置高度为100%*/
-    height: 100%;
+html,
+body,
+#app,
+.el-container {
+  /*设置内部填充为0，几个布局元素之间没有间距*/
+  padding: 0px;
+  /*外部间距也是如此设置*/
+  margin: 0px;
+  /*统一设置高度为100%*/
+  height: 100%;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-size: 12px;
@@ -24,5 +27,4 @@ html,body,#app,.el-container{
 // i{
 //   color: #409EFF;
 // }
-
 </style>
